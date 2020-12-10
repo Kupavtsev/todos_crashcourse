3b07c3daaa3cb8dca691adaf95ad3ff720a836f8
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-12-10 23:27:07 +0300

    Updates

---
+++ precache-manifest.8848384a9f7df9f6291ce4a579f7cabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d6073cf945d2f87303627c2685b7fa",
+    "url": "/todos_crashcourse/index.html"
+  },
+  {
+    "revision": "ee7c445b38080b73d717",
+    "url": "/todos_crashcourse/static/css/main.dcf23bd0.chunk.css"
+  },
+  {
+    "revision": "770bb9a3c4fab1c7cae0",
+    "url": "/todos_crashcourse/static/js/2.d3520680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos_crashcourse/static/js/2.d3520680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7c445b38080b73d717",
+    "url": "/todos_crashcourse/static/js/main.784a0463.chunk.js"
+  },
+  {
+    "revision": "58e52700e9dbe6410027",
+    "url": "/todos_crashcourse/static/js/runtime-main.6d181bf2.js"
+  }
+]);
